b0afc20fdf2ad54e2d39a040451c30771345a866
Ukanotas <simonas.rukas@gmail.com>
2020-09-29 11:31:37 +0300

    practice

---
+++ person.js
@@ -0,0 +1,14 @@
+/*
+name: Vardenis
+birthYear: 2002 m
+print: vardenis dabar yra 18 metu.
+*/
+
+function birth(name, birthYear) {
+let age = 2020 - birthYear;
+return `${name} dabar yra ${age}m`;
+
+}
+
+
+console.log(birth('vardenis', 2002));
